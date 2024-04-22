0c314af14033ed41e571a883cb2b5bf9eefb39eb
ibrvimv <ibrvimv@gmail.com>
2024-04-22 18:18:49 +0500

    подключил санити

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,13 +2,31 @@
  * This configuration is used to for the Sanity Studio that’s mounted on the `/app/studio/[[...index]]/page.tsx` route
  */
 
-import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
+import { visionTool } from '@sanity/vision';
+import { defineConfig } from 'sanity';
+import { structureTool } from 'sanity/structure';
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './sanity/env'
-import {schema} from './sanity/schema'
+import { apiVersion, dataset, projectId } from './sanity/env';
+import { schema } from './sanity/schema';
+import {
+  HomeIcon,
+  CaseIcon,
+  DocumentIcon,
+  PresentationIcon,
+  BulbOutlineIcon,
+  TiersIcon,
+  StarIcon,
+  EarthGlobeIcon,
+  EarthAmericasIcon,
+  CalendarIcon,
+  MenuIcon,
+  BillIcon,
+  EyeOpenIcon,
+  EnvelopeIcon,
+  FolderIcon,
+  FilterIcon,
+} from '@sanity/icons';
 
 export default defineConfig({
   basePath: '/studio',
@@ -17,9 +35,31 @@ export default defineConfig({
   // Add and edit the content schema in the './sanity/schema' folder
   schema,
   plugins: [
-    structureTool(),
+    structureTool({
+      structure: (S) => {
+        return S.list()
+          .title('PERSONAL WEBSITE ADMIN PANEL')
+          .items([
+            S.listItem()
+              .title('ABOUT ME')
+              .icon(HomeIcon)
+              .id('about')
+              .child(
+                S.editor()
+                  .title('ABOUT ME')
+                  .schemaType('about')
+                  .documentId('about')
+              ),
+            S.listItem()
+              .title('CV')
+              .icon(DocumentIcon)
+              .id('cv')
+              .child(S.editor().title('CV').schemaType('cv').documentId('cv')),
+          ]);
+      },
+    }),
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
+    visionTool({ defaultApiVersion: apiVersion }),
   ],
-})
+});
--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,10 +1,12 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from 'sanity';
 
-import blockContent from './schemaTypes/blockContent'
-import category from './schemaTypes/category'
-import post from './schemaTypes/post'
-import author from './schemaTypes/author'
+import blockContent from './schemaTypes/blockContent';
+import category from './schemaTypes/category';
+import post from './schemaTypes/post';
+import author from './schemaTypes/author';
+import about from './schemaTypes/about';
+import cv from './schemaTypes/cv';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, author, category, blockContent],
-}
+  types: [post, about, cv, author, category, blockContent],
+};
